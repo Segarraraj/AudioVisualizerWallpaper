a365b5b48029ad37c5155bd2b70678a871efe7b1
Segarraraj <segarraraj@esat-alumni.com>
2023-03-30 16:45:45 +0200

    Removed everything related to weather

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-var apikey = "";
-var cityname = "";
-var country = "";
-
-var lat = 0.0;
-var lon = 0.0;
-
 function livelyPropertyListener(name, val) {
 	switch(name) {
         case "bgImg":
@@ -13,41 +6,5 @@ function livelyPropertyListener(name, val) {
         case "bgBlur":
             document.getElementById("main").style.filter = "blur(" + val + "px)";
             break;
-        case "apikey":
-            apikey = val;
-            break;
-        case "cityname":
-            cityname = val;
-            getLatLon();
-            break;
-        case "country":
-            country = val;
-            getLatLon();
-            break;
   	}
-}
-
-function getLatLon() {
-    if (apikey == "" || cityname == "" || country == "") {
-        return;
-    }
-
-    var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname},${country}&appid=${apikey}`;
-    fetch(geoURL).then((response) => response.json()).then((data) => {
-        lat = data[0].lat;
-        lon = data[0].lon;
-
-        getWeather();
-    });
-}
-
-function getWeather() {
-    if (lat == 0.0 && lon == 0.0) {
-        return;
-    }
-
-    var weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}`;
-    fetch(weatherURL).then((response) => response.json()).then((data) => {
-        console.log(data);        
-    });
 }
